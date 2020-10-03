85c41d5b47ef834af67d5b391876e6017195ff6c
Philipp Lauwiner <philipp.lauwiner@treestones.ch>
2020-10-03 16:37:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# DailyHuddle (daily_huddle)
+
+Task Manager for daily huddles
+
+## Install the dependencies
+```bash
+yarn
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+yarn run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,99 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Daily Huddle Manager
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Report',
+    caption: 'Ready 4 Huddle',
+    icon: 'insert_chart',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Doing',
+    caption: 'Current Sprint',
+    icon: 'work',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Upcomming',
+    caption: 'Next Sprint',
+    icon: 'work_outline',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Backlog',
+    caption: 'Future Todos',
+    icon: 'toc',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Archive',
+    caption: 'Past/Done Tasks',
+    icon: 'archive',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Challenges',
+    caption: 'Those little basterds',
+    icon: 'warning',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Settings',
+    caption: 'Configurations',
+    icon: 'app_settings_alt',
+    link: 'https://facebook.quasar.dev'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
